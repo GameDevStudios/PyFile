5d53f2d40a474d883e0314638228ab94c3cd4f33
GameDevStudios <eamonnrea@gmail.com>
2013-11-26 20:15:39 +0000

    Added folder creation.

--- FilesystemHelper.py
+++ FilesystemHelper.py
@@ -12,7 +12,7 @@ class Filesystem():
 
     '''
 
-    A simple helper module for getting useful directories with ease.
+    A simple helper module for making folders and files.
 
     '''
 
@@ -44,8 +44,6 @@ class Filesystem():
 
         Returns your home directory.
 
-        Returns the path to your home directory.
-
         '''
 
         return self.homedir
@@ -53,13 +51,11 @@ class Filesystem():
     def file_exists(self, file_name):
         '''
 
-        Checks if a file exists.
+        Checks if a file exists. Returns True if a file exists, false otherwise.
 
         Takes 1 argument:
 
         file_name - The name of the file to check.
-
-        Returns True if a file exists, false otherwise.
 
         '''
 
@@ -68,7 +64,7 @@ class Filesystem():
     def pwd(self):
         '''
 
-        Returns the users current working directory
+        Returns the users current working directory. 
 
         '''
 
@@ -118,3 +114,37 @@ class Filesystem():
         self.working_dir = os.getcwd()
 
         return self.working_dir
+
+    def make_dir(self, folder_name, folder_path=None, pathtype='abs'):
+        '''
+
+        Creates a directory. Returns True if the directory already exists, returns the directory otherwise.
+
+        Takes 3 arguments:
+
+        folder_name - The name of the directory that you wish to create. Value must be provided.
+        folder_path - The path to the directory that you wish to create. If no value is provided it is defaulted to the current working directory.
+        pathtype - The type of path. Can be either 'abs' or 'rel'. If no value is provided it is defaulted to 'abs'.
+
+        '''
+
+        if folder_path is None:
+            folder_path = self.working_dir
+
+        if folder_name is None:
+            print "Sorry! A value must be provided for a directory name.\n\n"
+
+        if pathtype is 'abs':
+            if os.path.exists(os.path.join(folder_path, folder_name)):
+                return True
+            elif not os.path.exists(os.path.join(folder_path, folder_name)):
+                os.makedirs(os.path.join(folder_path, folder_name))
+
+                return folder_path
+        elif pathtype is 'rel':
+            if os.path.exists(os.path.join(self.working_dir, folder_name)):
+                return True
+            elif not os.path.exists(os.path.join(self.working_dir, folder_path)):
+                os.makedirs(os.path.join(self.working_dir, folder_path))
+
+                return str(os.path.join(self.working_dir, folder_path))
